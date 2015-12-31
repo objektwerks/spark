a214a2a0529baa4d16cc8d1df99c81606ef74404
objektwerks <objektwerks@gmail.com>
2015-12-31 17:20:53 -0500

    removed property setter

--- src/test/scala/spark/SparkInstance.scala
+++ src/test/scala/spark/SparkInstance.scala
@@ -6,7 +6,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 import scala.io.Source
 
 object SparkInstance {
-  val conf = new SparkConf().setMaster("local[2]").setAppName("sparky").set("spark.cassandra.connection.host", "127.0.0.1")
+  val conf = new SparkConf().setMaster("local[2]").setAppName("sparky")
   val context = new SparkContext(conf)
   val sqlContext = new SQLContext(context)
   val json = Source.fromInputStream(getClass.getResourceAsStream("/spark.json.txt")).getLines.toSeq
