9cf1a4ae7beb6bb596945cc71c3faf7d2e7fd058
objektwerks <objektwerks@gmail.com>
2016-02-03 09:05:08 -0500

    extracted regex

--- src/test/scala/spark/package.scala
+++ src/test/scala/spark/package.scala
@@ -2,11 +2,13 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.DStream
 
 package object spark {
+  val regex = "\\P{L}+"
+
   def countWords(rdd: RDD[String]): RDD[(String, Int)] = {
-    rdd.flatMap(l => l.split("\\P{L}+")).filter(_.nonEmpty).map(_.toLowerCase).map(w => (w, 1)).reduceByKey(_ + _)
+    rdd.flatMap(l => l.split(regex)).filter(_.nonEmpty).map(_.toLowerCase).map(w => (w, 1)).reduceByKey(_ + _)
   }
 
   def countWords(ds: DStream[String]): DStream[(String, Int)] = {
-    ds.flatMap(l => l.split("\\P{L}+")).filter(_.nonEmpty).map(_.toLowerCase).map(w => (w, 1)).reduceByKey(_ + _)
+    ds.flatMap(l => l.split(regex)).filter(_.nonEmpty).map(_.toLowerCase).map(w => (w, 1)).reduceByKey(_ + _)
   }
 }
