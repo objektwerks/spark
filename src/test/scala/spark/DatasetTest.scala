15224d98dca592203f5cdc2bd1600fbd21fee310
objektwerks <objektwerks@runbox.com>
2016-12-16 08:20:00 -0500

    enhanced dataset test

--- src/test/scala/spark/DatasetTest.scala
+++ src/test/scala/spark/DatasetTest.scala
@@ -6,10 +6,19 @@ class DatasetTest extends FunSuite {
   val session = SparkInstance.sparkSession
   val context = SparkInstance.sparkSession.sparkContext
 
-  test("json > case class > dataset") {
+  test("dataset") {
     import session.implicits._
     val dataset = session.read.json(context.makeRDD(SparkInstance.personJson)).as[Person]
+
     assert(dataset.count == 4)
     assert(dataset.filter(_.age == 24).first.name == "fred")
+
+    val names = dataset.select("name").orderBy("name").collect
+    assert(names.length == 4)
+    assert(names.head.mkString == "barney")
+
+    val ages = dataset.select("age").orderBy("age").collect
+    assert(ages.length == 4)
+    assert(ages.head.getLong(0) == 21)
   }
 }
