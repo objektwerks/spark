6b5b73bba71d6bdbd2013d43a8f981c7efca0654
objektwerks <objektwerks@runbox.com>
2017-08-22 10:00:29 -0400

    refactored ratings test

--- src/test/scala/spark/RatingsTest.scala
+++ src/test/scala/spark/RatingsTest.scala
@@ -1,18 +1,22 @@
 package spark
 
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
 import scala.io.Source
 
-class RatingsTest extends FunSuite {
+class RatingsTest extends FunSuite with Matchers {
   import SparkInstance._
 
   test("count") {
     // u.data = ( userid, movieid, rating, timestamp )
     val data = Source.fromInputStream(this.getClass.getResourceAsStream("/ratings/u.data")).getLines.toSeq
     val lines = sparkContext.makeRDD(data).cache
-    val ratings = lines.map(line => line.split("\t")(2))
+    val ratings = lines.map(line => line.split("\t")(2).toInt)
     val ratingsByCount = ratings.countByValue
-    ratingsByCount.toSeq.sortBy(_._1).foreach(println)
+    ratingsByCount(1) shouldBe 6110
+    ratingsByCount(2) shouldBe 11370
+    ratingsByCount(3) shouldBe 27145
+    ratingsByCount(4) shouldBe 34174
+    ratingsByCount(5) shouldBe 21201
   }
 }
