56530db1c58d3f6896502f909147f98a71bdd096
objektwerks <objektwerks@runbox.com>
2017-08-24 10:02:55 -0400

    merged test files

--- src/test/scala/spark/RddTest.scala
+++ src/test/scala/spark/RddTest.scala
@@ -1,11 +1,12 @@
 package spark
 
 import org.apache.spark.HashPartitioner
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
 import scala.collection.SortedSet
+import scala.io.Source
 
-class RddTest extends FunSuite {
+class RddTest extends FunSuite with Matchers {
   import SparkInstance._
 
   test("transformations with action") {
@@ -129,5 +130,33 @@ class RddTest extends FunSuite {
     val maxWordCount = wordCountRdd.values.max
     val (word, count) = wordCountRdd.filter(_._2 == maxWordCount).first
     assert(word == "the" && count == 14)
+  }
+
+  test("count") {
+    val data = Source.fromInputStream(this.getClass.getResourceAsStream("/ratings/u.data")).getLines.toSeq
+    val lines = sparkContext.makeRDD(data)
+    val ratings = lines.map(line => line.split("\t")(2).toInt)
+    val ratingsByCount = ratings.countByValue
+    ratingsByCount(1) shouldBe 6110
+    ratingsByCount(2) shouldBe 11370
+    ratingsByCount(3) shouldBe 27145
+    ratingsByCount(4) shouldBe 34174
+    ratingsByCount(5) shouldBe 21201
+  }
+
+  test("average") {
+    def parseLine(line: String): (Int, Int) = {
+      val fields = line.split(",")
+      (fields(2).toInt, fields(3).toInt)
+    }
+
+    val data = Source.fromInputStream(this.getClass.getResourceAsStream("/friends.txt")).getLines.toSeq
+    val lines = sparkContext.makeRDD(data)
+    val rdd = lines.map(parseLine)
+    val totalsByAge = rdd.mapValues(x => (x, 1)).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)) // (age, (friends, 1))
+    val averagesByAge = totalsByAge.mapValues(x => x._1 / x._2) // (age, friends)
+    val results = averagesByAge.collect.sorted
+    (18, 343) shouldEqual results.head
+    (69, 235) shouldEqual results.last
   }
 }
