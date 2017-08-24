1d6bc715d7720dc333c292b5708d90f20d5a7a4c
objektwerks <objektwerks@runbox.com>
2017-08-24 07:51:53 -0400

    added average test and data

--- src/test/scala/spark/RatingsTest.scala
+++ src/test/scala/spark/RatingsTest.scala
@@ -8,7 +8,6 @@ class RatingsTest extends FunSuite with Matchers {
   import SparkInstance._
 
   test("count") {
-    // u.data = ( userid, movieid, rating, timestamp )
     val data = Source.fromInputStream(this.getClass.getResourceAsStream("/ratings/u.data")).getLines.toSeq
     val lines = sparkContext.makeRDD(data)
     val ratings = lines.map(line => line.split("\t")(2).toInt)
@@ -18,5 +17,21 @@ class RatingsTest extends FunSuite with Matchers {
     ratingsByCount(3) shouldBe 27145
     ratingsByCount(4) shouldBe 34174
     ratingsByCount(5) shouldBe 21201
+  }
+
+  test("average") {
+    def parseLine(line: String): (Int, Int) = {
+      val fields = line.split(",")
+      (fields(2).toInt, fields(3).toInt)
+    }
+
+    val data = Source.fromInputStream(this.getClass.getResourceAsStream("/friends.csv")).getLines.toSeq
+    val lines = sparkContext.makeRDD(data)
+    val rdd = lines.map(parseLine)
+    val totalsByAge = rdd.mapValues(x => (x, 1)).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)) // (age, (friends, 1))
+    val averagesByAge = totalsByAge.mapValues(x => x._1 / x._2) // (age, friends)
+    val results = averagesByAge.collect.sorted
+    (18, 343) shouldEqual results.head
+    (69, 235) shouldEqual results.last
   }
 }
