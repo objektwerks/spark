e0c6a32466cd9563b469be98af82315eb090c3bb
objektwerks <objektwerks@runbox.com>
2017-08-26 06:22:11 -0400

    refactored logging

--- src/test/scala/spark/SparkInstance.scala
+++ src/test/scala/spark/SparkInstance.scala
@@ -1,10 +1,13 @@
 package spark
 
+import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 
 import scala.io.Source
 
 object SparkInstance {
+  val logger = Logger.getLogger(this.getClass)
+
   val sparkSession = SparkSession.builder
     .master("local[2]")
     .appName("sparky")
@@ -12,6 +15,10 @@ object SparkInstance {
   val sparkContext = sparkSession.sparkContext
   val personJson = Source.fromInputStream(getClass.getResourceAsStream("/person.json.txt")).getLines.toSeq
   val licenseText = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).getLines.toSeq
+  logger.info("*** Initialized spark instance.")
 
-  sys.addShutdownHook { sparkSession.stop() }
+  sys.addShutdownHook {
+    sparkSession.stop()
+    logger.info("*** Terminated spark instance.")
+  }
 }
