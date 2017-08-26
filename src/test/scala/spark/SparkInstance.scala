0474b80883a99873234b7fe592a85a9fa47dc436
objektwerks <objektwerks@runbox.com>
2017-08-26 06:15:11 -0400

    added sys shutdown hook to spark instance

--- src/test/scala/spark/SparkInstance.scala
+++ src/test/scala/spark/SparkInstance.scala
@@ -12,4 +12,6 @@ object SparkInstance {
   val sparkContext = sparkSession.sparkContext
   val personJson = Source.fromInputStream(getClass.getResourceAsStream("/person.json.txt")).getLines.toSeq
   val licenseText = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).getLines.toSeq
+
+  sys.addShutdownHook { sparkSession.stop() }
 }
