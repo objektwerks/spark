c8343fde1c4c18ae626c7f10ab50e93cc262e51b
objektwerks <objektwerks@runbox.com>
2017-09-02 17:08:01 -0400

    refactoring log entry app

--- src/main/scala/spark/LogEntryApp.scala
+++ src/main/scala/spark/LogEntryApp.scala
@@ -8,36 +8,29 @@ import org.apache.spark.sql.functions.window
 import scala.io.Codec
 
 object LogEntryApp extends App {
-  implicit val codec = Codec("UTF-8")
-  codec.onMalformedInput(CodingErrorAction.REPLACE)
-  codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
   val sparkSession = SparkSession.builder.master("local[2]").appName("sparky").getOrCreate()
 
-  import LogEntry._
   import LogEntryParser._
   import sparkSession.implicits._
 
+  implicit val codec = Codec("UTF-8")
+  codec.onMalformedInput(CodingErrorAction.REPLACE)
+  codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
   val reader = sparkSession.readStream.text("./data/log")
-  val logEntries = reader.flatMap(parseRow).cache
 
-  val statusDateTime = logEntries
+  val logEntries = reader
+    .flatMap(parseRow)
     .select("status", "dateTime")
     .groupBy($"status", window($"dateTime", "1 hour"))
     .count
     .orderBy("window")
-  val statusDateTimeWriter = statusDateTime
+
+  val logEntryWriter = logEntries
     .writeStream
     .outputMode("complete")
     .format("console")
     .start
-
-  val logEntryWriter = logEntries
-    .writeStream
-    .foreach(logEntryForeachWriter)
-    .start
-
-  statusDateTimeWriter.awaitTermination(60000)
-  logEntryWriter.awaitTermination(60000)
+  logEntryWriter.awaitTermination()
 
   sparkSession.stop
 }
