bd5a667a6d2b9d45337f936cd614529ff1a78473
objektwerks <objektwerks@runbox.com>
2017-09-12 19:06:26 -0400

    refactored entity into person and age

--- src/test/scala/spark/Person.scala
+++ src/test/scala/spark/Person.scala
@@ -16,16 +16,3 @@ object Person {
     override def close(errorOrNull: Throwable): Unit = logger.info("*** Closing person foreach writer...")
   }
 }
-
-case class Age(age: Long = 0) extends AnyVal {
-  implicit def +(other: Age): Age = Age(age + other.age)
-}
-
-object Age {
-  class Average(ages: Array[Age]) {
-    def avg: Age = Age( ages.reduce( ( a, b ) => a + b ).age / ages.length )
-  }
-  implicit def ordering: Ordering[Age] = Ordering.by(_.age)
-  implicit def avg(ages: Array[Age]) = new Average(ages)
-  implicit def sum(ages: Array[Age]) = ages.reduce(_ + _)
-}
