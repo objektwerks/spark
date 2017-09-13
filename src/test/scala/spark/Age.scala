06709b16dc852891357688fa1ef0b2a50260df16
objektwerks <objektwerks@runbox.com>
2017-09-13 08:18:22 -0400

    refactored Age

--- src/test/scala/spark/Age.scala
+++ src/test/scala/spark/Age.scala
@@ -6,7 +6,7 @@ case class Age(age: Long = 0) extends AnyVal {
 
 object Age {
   class Average(ages: Array[Age]) {
-    def avg: Age = Age( ages.reduce( ( a, b ) => a + b ).age / ages.length )
+    def avg: Age = Age(ages.reduce(_ + _).age / ages.length)
   }
   implicit def ordering: Ordering[Age] = Ordering.by(_.age)
   implicit def avg(ages: Array[Age]) = new Average(ages)
