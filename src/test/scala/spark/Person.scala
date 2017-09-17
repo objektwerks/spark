155c1410c44bbfd0b78c88aea7e593564baa7fd2
objektwerks <objektwerks@runbox.com>
2017-09-17 07:55:13 -0400

    added role to Person

--- src/test/scala/spark/Person.scala
+++ src/test/scala/spark/Person.scala
@@ -4,14 +4,14 @@ import org.apache.log4j.Logger
 import org.apache.spark.sql.ForeachWriter
 import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 
-case class Person(age: Long, name: String)
+case class Person(age: Long, name: String, role: String)
 
 object Person {
   val logger = Logger.getLogger(this.getClass)
 
   implicit def ordering: Ordering[Person] = Ordering.by(_.age)
 
-  val personStructType = new StructType().add("age", IntegerType).add("name", StringType)
+  val personStructType = new StructType().add("age", IntegerType).add("name", StringType).add("role", StringType)
 
   val personForeachWriter = new ForeachWriter[Person] {
     override def open(partitionId: Long, version: Long): Boolean = true
