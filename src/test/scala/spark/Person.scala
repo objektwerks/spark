39df9902c3897513ce1790ee6328e4ea57e7f9ef
objektwerks <objektwerks@runbox.com>
2017-09-17 07:59:12 -0400

    refactored Person code org

--- src/test/scala/spark/Person.scala
+++ src/test/scala/spark/Person.scala
@@ -6,22 +6,18 @@ import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 
 case class Person(age: Long, name: String, role: String)
 
+class Average(ages: Array[Long]) {
+  def avg: Double = ages.sum / ages.length.toDouble
+}
+
 object Person {
   val logger = Logger.getLogger(this.getClass)
-
-  implicit def ordering: Ordering[Person] = Ordering.by(_.age)
-
   val personStructType = new StructType().add("age", IntegerType).add("name", StringType).add("role", StringType)
-
   val personForeachWriter = new ForeachWriter[Person] {
     override def open(partitionId: Long, version: Long): Boolean = true
     override def process(person: Person): Unit = logger.info(s"*** $person")
     override def close(errorOrNull: Throwable): Unit = logger.info("*** Closing person foreach writer...")
   }
-
-  class Average(ages: Array[Long]) {
-    def avg: Double = ages.sum / ages.length.toDouble
-  }
-
+  implicit def ordering: Ordering[Person] = Ordering.by(_.age)
   implicit def avg(ages: Array[Long]) = new Average(ages)
 }
