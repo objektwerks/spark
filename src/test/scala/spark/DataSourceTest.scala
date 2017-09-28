2b785d281bf3d23229a8bf335b04cb999f720e06
objektwerks <objektwerks@runbox.com>
2017-09-28 13:47:01 -0400

    added json data source test

--- src/test/scala/spark/DataSourceTest.scala
+++ src/test/scala/spark/DataSourceTest.scala
@@ -1,13 +1,23 @@
 package spark
 
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Dataset, Row}
 import org.scalatest.{FunSuite, Matchers}
 
 class DataSourceTest extends FunSuite with Matchers {
   import SparkInstance._
+  import sparkSession.implicits._
 
   test("text") {
     val lines: RDD[String] = sparkContext.textFile("./data/txt/license.txt")
     lines.count shouldBe 19
+  }
+
+  test("json") {
+    val dataset: Dataset[Person] = sparkSession.read.json("./data/json/person.json").as[Person]
+    dataset.count shouldBe 4
+
+    val dataframe: Dataset[Row] = sparkSession.read.json("./data/json/person.json")
+    dataframe.count shouldBe 4
   }
 }
