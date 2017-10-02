07df46d32ffdbadef774856e8dd11c20af3b5cb6
objektwerks <objektwerks@runbox.com>
2017-10-02 07:53:46 -0400

    added broadcast test

---
+++ src/test/scala/spark/BroadcastTest.scala
@@ -0,0 +1,15 @@
+package spark
+
+import org.scalatest.{FunSuite, Matchers}
+
+class BroadcastTest extends FunSuite with Matchers {
+  import SparkInstance._
+
+  test("broadcast") {
+    val map = Map("fred" -> "wilma", "barney" -> "betty")
+    val broadcastMap = sparkContext.broadcast(map)
+    val wives = sparkContext.parallelize(Seq("fred", "barney")).map(broadcastMap.value).collect
+    wives(0) shouldBe "wilma"
+    wives(1) shouldBe "betty"
+  }
+}
