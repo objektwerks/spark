7d3867c38ea7b330564e9107c26cd2516656acbc
objektwerks <objektwerks@runbox.com>
2017-10-08 11:54:04 -0400

    refactored spark app listener logging

--- src/main/scala/spark/SparkAppListener.scala
+++ src/main/scala/spark/SparkAppListener.scala
@@ -1,6 +1,7 @@
 package spark
 
 import org.apache.log4j.Logger
+import org.apache.spark.executor.TaskMetrics
 import org.apache.spark.scheduler._
 
 import scala.collection.mutable.ListBuffer
@@ -9,7 +10,7 @@ class SparkAppListener extends SparkListener {
   val events = ListBuffer[String]()
 
   def log(): Unit = {
-    val logger = Logger.getLogger(this.getClass)
+    val logger = Logger.getLogger("SparkAppListener")
     events foreach { event => logger.info(event) }
   }
 
@@ -41,11 +42,14 @@ class SparkAppListener extends SparkListener {
 
   override def onBlockUpdated(updated: SparkListenerBlockUpdated): Unit = events += s"*** On block updated: ${updated.toString}"
 
-  override def onTaskStart(start: SparkListenerTaskStart) = events += s"*** On task start: ${start.toString}"
+  override def onTaskStart(start: SparkListenerTaskStart): Unit = events += s"*** On task start - task info: ${taskInfoToString(start.taskInfo)}"
 
-  override def onTaskEnd(end: SparkListenerTaskEnd) = events += s"*** On task end: ${end.toString}"
+  override def onTaskEnd(end: SparkListenerTaskEnd): Unit = {
+    events += s"*** On task start - task info: ${taskInfoToString(end.taskInfo)}"
+    events += s"*** On task start - task metrics: ${taskMetricsToString(end.taskMetrics)}"
+  }
 
-  override def onTaskGettingResult(result: SparkListenerTaskGettingResult): Unit = events += s"*** On task result: ${result.toString}"
+  override def onTaskGettingResult(result: SparkListenerTaskGettingResult): Unit = events += s"*** On task result: ${taskInfoToString(result.taskInfo)}"
 
   override def onUnpersistRDD(unpersist: SparkListenerUnpersistRDD): Unit = events += s"*** On rdd unpersisted: ${unpersist.toString}"
 
@@ -56,4 +60,31 @@ class SparkAppListener extends SparkListener {
   override def onEnvironmentUpdate(update: SparkListenerEnvironmentUpdate): Unit = events += s"*** On env update: ${update.toString}"
 
   override def onOtherEvent(event: SparkListenerEvent): Unit = events += s"*** On other event: ${event.toString}"
+
+  def taskInfoToString(taskInfo: TaskInfo): String = {
+    val info = ListBuffer[String]()
+    info += s"host: ${taskInfo.host}"
+    info += s"executor id: ${taskInfo.executorId}"
+    info += s"task id: ${taskInfo.taskId}"
+    info += s"running?: ${taskInfo.running}"
+    info += s"status: ${taskInfo.status}"
+    info += s"attempt number: ${taskInfo.attemptNumber}"
+    info += s"launch time: ${taskInfo.launchTime}"
+    info += s"finish time: ${taskInfo.finishTime}"
+    info += s"result time: ${taskInfo.gettingResultTime}"
+    info += s"duration: ${taskInfo.duration}"
+    info += s"getting result?: ${taskInfo.gettingResult}"
+    info += s"successful?: ${taskInfo.successful}"
+    info += s"failed?: ${taskInfo.failed}"
+    info += s"killed?: ${taskInfo.killed}"
+    info += s"accumuables: ${taskInfo.accumulables.foreach(println)}"
+    info.mkString
+  }
+
+  def taskMetricsToString(taskMetrics: TaskMetrics): String = {
+    val info = ListBuffer[String]()
+    info += s"executor cpu time: ${taskMetrics.executorCpuTime}"
+    info += s"result size: ${taskMetrics.resultSize}"
+    info.mkString
+  }
 }
