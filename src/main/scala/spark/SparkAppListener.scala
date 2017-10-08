6f6e3b87c5975d461db83e36b48dcbf75748b009
objektwerks <objektwerks@runbox.com>
2017-10-08 10:58:14 -0400

    refactored spark app listener

--- src/main/scala/spark/SparkAppListener.scala
+++ src/main/scala/spark/SparkAppListener.scala
@@ -3,47 +3,47 @@ package spark
 import org.apache.spark.scheduler._
 
 class SparkAppListener extends SparkListener {
-  override def onApplicationStart(start: SparkListenerApplicationStart): Unit = super.onApplicationStart(start)
+  override def onApplicationStart(start: SparkListenerApplicationStart): Unit = println(s"On app start: ${start.toString}")
 
-  override def onApplicationEnd(end: SparkListenerApplicationEnd): Unit = super.onApplicationEnd(end)
+  override def onApplicationEnd(end: SparkListenerApplicationEnd): Unit = println(s"On app end: ${end.toString}")
 
-  override def onJobStart(start: SparkListenerJobStart): Unit = super.onJobStart(start)
+  override def onJobStart(start: SparkListenerJobStart): Unit = println(s"On job start: ${start.toString}")
 
-  override def onJobEnd(end: SparkListenerJobEnd): Unit = super.onJobEnd(end)
+  override def onJobEnd(end: SparkListenerJobEnd): Unit = println(s"On job end: ${end.toString}")
 
-  override def onStageSubmitted(submitted: SparkListenerStageSubmitted): Unit = super.onStageSubmitted(submitted)
+  override def onStageSubmitted(submitted: SparkListenerStageSubmitted): Unit = println(s"On stage submitted: ${submitted.toString}")
 
-  override def onStageCompleted(completed: SparkListenerStageCompleted): Unit = super.onStageCompleted(completed)
+  override def onStageCompleted(completed: SparkListenerStageCompleted): Unit = println(s"On stage completed: ${completed.toString}")
 
-  override def onExecutorMetricsUpdate(update: SparkListenerExecutorMetricsUpdate): Unit = super.onExecutorMetricsUpdate(update)
+  override def onExecutorMetricsUpdate(update: SparkListenerExecutorMetricsUpdate): Unit = println(s"On executor update: ${update.toString}")
 
-  override def onExecutorAdded(added: SparkListenerExecutorAdded): Unit = super.onExecutorAdded(added)
+  override def onExecutorAdded(added: SparkListenerExecutorAdded): Unit = println(s"On executor added: ${added.toString}")
 
-  override def onExecutorRemoved(removed: SparkListenerExecutorRemoved): Unit = super.onExecutorRemoved(removed)
+  override def onExecutorRemoved(removed: SparkListenerExecutorRemoved): Unit = println(s"On executor removed: ${removed.toString}")
 
-  override def onExecutorBlacklisted(blacklisted: SparkListenerExecutorBlacklisted): Unit = super.onExecutorBlacklisted(blacklisted)
+  override def onExecutorBlacklisted(blacklisted: SparkListenerExecutorBlacklisted): Unit = println(s"On executor blacklisted: ${blacklisted.toString}")
 
-  override def onExecutorUnblacklisted(unblacklisted: SparkListenerExecutorUnblacklisted): Unit = super.onExecutorUnblacklisted(unblacklisted)
+  override def onExecutorUnblacklisted(unblacklisted: SparkListenerExecutorUnblacklisted): Unit = println(s"On executor unblacklisted: ${unblacklisted.toString}")
 
-  override def onBlockManagerAdded(added: SparkListenerBlockManagerAdded): Unit = super.onBlockManagerAdded(added)
+  override def onBlockManagerAdded(added: SparkListenerBlockManagerAdded): Unit = println(s"On block manager added: ${added.toString}")
 
-  override def onBlockManagerRemoved(removed: SparkListenerBlockManagerRemoved): Unit = super.onBlockManagerRemoved(removed)
+  override def onBlockManagerRemoved(removed: SparkListenerBlockManagerRemoved): Unit = println(s"On block manager removed: ${removed.toString}")
 
-  override def onBlockUpdated(updated: SparkListenerBlockUpdated): Unit = super.onBlockUpdated(updated)
+  override def onBlockUpdated(updated: SparkListenerBlockUpdated): Unit = println(s"On block updated: ${updated.toString}")
 
   override def onTaskStart(start: SparkListenerTaskStart) = println(s"On task start: ${start.toString}")
 
   override def onTaskEnd(end: SparkListenerTaskEnd) = println(s"On task end: ${end.toString}")
 
-  override def onTaskGettingResult(result: SparkListenerTaskGettingResult): Unit = super.onTaskGettingResult(result)
+  override def onTaskGettingResult(result: SparkListenerTaskGettingResult): Unit = println(s"On task result: ${result.toString}")
 
-  override def onUnpersistRDD(unpersist: SparkListenerUnpersistRDD): Unit = super.onUnpersistRDD(unpersist)
+  override def onUnpersistRDD(unpersist: SparkListenerUnpersistRDD): Unit = println(s"On rdd unpersisted: ${unpersist.toString}")
 
-  override def onNodeBlacklisted(blacklisted: SparkListenerNodeBlacklisted): Unit = super.onNodeBlacklisted(blacklisted)
+  override def onNodeBlacklisted(blacklisted: SparkListenerNodeBlacklisted): Unit = println(s"On node blacklisted: ${blacklisted.toString}")
 
-  override def onNodeUnblacklisted(unblacklisted: SparkListenerNodeUnblacklisted): Unit = super.onNodeUnblacklisted(unblacklisted)
+  override def onNodeUnblacklisted(unblacklisted: SparkListenerNodeUnblacklisted): Unit = println(s"On node unblacklisted: ${unblacklisted.toString}")
 
-  override def onEnvironmentUpdate(update: SparkListenerEnvironmentUpdate): Unit = super.onEnvironmentUpdate(update)
+  override def onEnvironmentUpdate(update: SparkListenerEnvironmentUpdate): Unit = println(s"On env update: ${update.toString}")
 
-  override def onOtherEvent(event: SparkListenerEvent): Unit = super.onOtherEvent(event)
+  override def onOtherEvent(event: SparkListenerEvent): Unit = println(s"On other event: ${event.toString}")
 }
