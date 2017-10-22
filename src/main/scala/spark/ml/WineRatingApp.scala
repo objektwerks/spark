20509982b6f898b2bca76bb607d2734044a598a3
objektwerks <objektwerks@runbox.com>
2017-10-22 11:37:14 -0400

    added wine ratings data and wine rating app - still a work in progress!

---
+++ src/main/scala/spark/ml/WineRatingApp.scala
@@ -0,0 +1,65 @@
+package spark.ml
+
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.ml.regression.GBTRegressor
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
+
+import spark.SparkInstance
+
+object WineRatingApp extends App {
+  import SparkInstance._
+
+  // Data.
+  val schemaStruct = StructType(
+    StructField("points", DoubleType) ::
+      StructField("country", StringType) ::
+      StructField("price", DoubleType) :: Nil
+  )
+  val dataframe = sparkSession.read.format("csv").option("header", "true").schema(schemaStruct).load("./data/wine/*.csv").na.drop()
+  val Array(trainingData, testData) = dataframe.randomSplit(Array(0.8, 0.2))
+  val labelColumn = "price"
+
+  // Category Indexer.
+  val countryIndexer = new StringIndexer()
+    .setInputCol("country")
+    .setOutputCol("countryIndex")
+
+  // Features assembler.
+  val featuresAssembler = new VectorAssembler()
+    .setInputCols(Array("points", "countryIndex"))
+    .setOutputCol("features")
+
+  // Estimator - gradient-boosted tree estimator.
+  val gradientBoostedTreeEstimator = new GBTRegressor()
+    .setLabelCol(labelColumn)
+    .setFeaturesCol("features")
+    .setPredictionCol("Predicted " + labelColumn)
+    .setMaxIter(50)
+
+  // Stages.
+  val stages = Array(countryIndexer, featuresAssembler, gradientBoostedTreeEstimator)
+
+  // Pipeline.
+  val pipeline = new Pipeline().setStages(stages)
+
+  // Model.
+  val model = pipeline.fit(trainingData)
+
+  // Predictions.
+  val predictions = model.transform(testData)
+
+  // Evaluator - evaluate the error/deviation of the regression using the Root Mean Squared deviation.
+  val evaluator = new RegressionEvaluator()
+    .setLabelCol(labelColumn)
+    .setPredictionCol("Predicted " + labelColumn)
+    .setMetricName("rmse")
+
+  // Evaluate!
+  val error = evaluator.evaluate(predictions)
+
+  println(error)
+  sparkListener.log()
+  sparkSession.stop()
+}
