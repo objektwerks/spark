9274d60000386f1886dad36db9d55eccb2cf7fd8
objektwerks <objektwerks@runbox.com>
2019-02-21 13:19:23 -0500

    added gradle support

--- README.md
+++ README.md
@@ -2,9 +2,17 @@ Spark
 -----
 >The purpose of the project is to test Spark features.
 
+Install
+-------
+1. brew install sbt **and/or** brew install gradle
+
 Test
 ----
-1. sbt clean test
+1. sbt clean test  **and/or** gradle clean test
+
+>Running **sbt clean test** takes about 51 seconds, while **gradle clean test** takes about 42 seconds.
+
+>See **/spark/build/reports/tests/test/index.html** for gradle test result report.
 
 Run
 ---
@@ -36,3 +44,9 @@ Here's a few examples:
 --------
 1. Create an .sbtopts file in the project root directory.
 2. Add this line ( to line 1 ): -java-home /Library/Java/JavaVirtualMachines/jdk1.8.0_192.jdk/Contents/Home
+
+Eclipse
+-------
+1. gradle eclipse
+
+>See .gitignore for Eclipse files excluded from Git.
---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    scala
+    eclipse
+    id("com.github.maiflai.scalatest") version "0.23"
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    implementation("org.scala-lang:scala-library:2.12.8")
+    implementation("org.apache.spark:spark-core_2.12:2.4.0")
+    implementation("org.apache.spark:spark-streaming_2.12:2.4.0")
+    implementation("org.apache.spark:spark-sql_2.12:2.4.0")
+    implementation("org.apache.spark:spark-mllib_2.12:2.4.0")
+    implementation("org.apache.spark:spark-graphx_2.12:2.4.0")
+    implementation("org.scalikejdbc:scalikejdbc_2.12:3.3.2")
+    implementation("com.h2database:h2:1.4.197")
+    implementation("org.slf4j:slf4j-api:1.7.25")
+    testImplementation("org.scalatest:scalatest_2.12:3.0.5")
+    testRuntime("org.pegdown:pegdown:1.4.2")
+}
