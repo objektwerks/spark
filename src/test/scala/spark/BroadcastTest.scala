1fda3ddd4d9359ef6ae0ef7c19199d1c9ef80e5a
objektwerks <objektwerks@runbox.com>
2019-07-30 12:38:45 -0400

    refactored broadcast test to use join

--- src/test/scala/spark/BroadcastTest.scala
+++ src/test/scala/spark/BroadcastTest.scala
@@ -4,12 +4,18 @@ import org.scalatest.{FunSuite, Matchers}
 
 class BroadcastTest extends FunSuite with Matchers {
   import SparkInstance._
+  import sparkSession.implicits._
 
-  test("broadcast") {
-    val map = Map("fred" -> "wilma", "barney" -> "betty")
-    val broadcastMap = sparkContext.broadcast(map)
-    val wives = sparkContext.parallelize(seq = Seq("fred", "barney")).map(broadcastMap.value).collect
-    wives(0) shouldBe "wilma"
-    wives(1) shouldBe "betty"
+  test("broadcast join") {
+    val broadcastPersons = sparkContext.broadcast(sparkSession.read.json("./data/person/person.json").as[Person])
+
+    val persons = broadcastPersons.value
+    val tasks = sparkSession.read.json("./data/task/task.json").as[Task]
+
+    val joinBy = broadcastPersons.value.col("id") === tasks.col("pid")
+    val personsTasks = persons.join(tasks, joinBy)
+
+    personsTasks.count shouldBe 4
+    personsTasks.show
   }
 }
