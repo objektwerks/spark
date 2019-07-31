600defc51686e9426b6f2ea1f7f3996f2a9ebf9e
objektwerks <objektwerks@runbox.com>
2019-07-31 04:06:25 -0400

    refactored accumulator test

--- src/test/scala/spark/AccumulatorTest.scala
+++ src/test/scala/spark/AccumulatorTest.scala
@@ -1,15 +1,12 @@
 package spark
 
-import org.apache.spark.util.LongAccumulator
 import org.scalatest.{FunSuite, Matchers}
 
 class AccumulatorTest extends FunSuite with Matchers {
   import SparkInstance._
 
   test("accumulator") {
-    val accumulator = new LongAccumulator()
-    val accumulatorName = "longAccumulator"
-    sparkContext.register(accumulator, accumulatorName)
+    val accumulator = sparkContext.longAccumulator(name = "accumulator")
     accumulator.add(1)
     accumulator.value shouldBe 1
   }
