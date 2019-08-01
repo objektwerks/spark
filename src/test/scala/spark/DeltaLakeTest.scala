2c843522b6b3145b5c131fc115f482e8c57e592f
objektwerks <objektwerks@runbox.com>
2019-08-01 15:47:20 -0400

    reordered code

--- src/test/scala/spark/DeltaLakeTest.scala
+++ src/test/scala/spark/DeltaLakeTest.scala
@@ -4,10 +4,10 @@ import org.scalatest.{FunSuite, Matchers}
 
 class DeltaLakeTest extends FunSuite with Matchers {
   import SparkInstance._
-  val personsPath = "./target/delta/persons"
-  val personsDataframe = sparkSession.read.json("./data/person/person.json")
 
   test("write -> read") {
+    val personsPath = "./target/delta/persons"
+    val personsDataframe = sparkSession.read.json("./data/person/person.json")
     personsDataframe.write.format("delta").mode("overwrite").save(personsPath)
     val personsDelta = sparkSession.read.format("delta").load(personsPath)
     personsDelta.select("*").show
