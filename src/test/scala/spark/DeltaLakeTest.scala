8560a334e2bf150e55af944ddfdee65a6522f289
objektwerks <objektwerks@runbox.com>
2019-08-01 15:33:07 -0400

    implemented batch write -> read test in data lake test

--- src/test/scala/spark/DeltaLakeTest.scala
+++ src/test/scala/spark/DeltaLakeTest.scala
@@ -4,8 +4,12 @@ import org.scalatest.{FunSuite, Matchers}
 
 class DeltaLakeTest extends FunSuite with Matchers {
   import SparkInstance._
+  val personsPath = "./target/delta/persons"
+  val personsDataframe = sparkSession.read.json("./data/person/person.json")
 
-  test("lake") {
-    println(s"Spark User: ${sparkContext.sparkUser}")
+  test("write -> read") {
+    personsDataframe.write.format("delta").mode("overwrite").save(personsPath)
+    val personsDelta = sparkSession.read.format("delta").load(personsPath)
+    personsDelta.select("*").show
   }
 }
