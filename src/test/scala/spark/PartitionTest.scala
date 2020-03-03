d33f50c12ae72b7a7b2fd5fd1e2d87337e48a0a7
objektwerks <objektwerks@runbox.com>
2020-03-03 13:15:56 -0500

    added repartition test to partition test

--- src/test/scala/spark/PartitionTest.scala
+++ src/test/scala/spark/PartitionTest.scala
@@ -6,7 +6,7 @@ class PartitionTest extends FunSuite with Matchers {
   import SparkInstance._
   import sparkSession.implicits._
 
-  test("partitions") {
+  test("partition") {
     val dataframe = (1 to 10).toList.toDF("number")
     dataframe.rdd.partitions.length shouldEqual 8
     dataframe.write.csv("./target/partitioned-numbers")
@@ -18,5 +18,13 @@ class PartitionTest extends FunSuite with Matchers {
     val coalesced = dataframe.coalesce(2)
     coalesced.rdd.partitions.length shouldEqual 2
     coalesced.write.csv("./target/coalesced-numbers")
+  }
+
+  test("repartition") {
+    val dataframe = (1 to 10).toList.toDF("number")
+    dataframe.rdd.partitions.length shouldEqual 8
+    dataframe.repartition(6).rdd.partitions.length shouldEqual 6
+    dataframe.repartition(4).rdd.partitions.length shouldEqual 4
+    dataframe.repartition(2).rdd.partitions.length shouldEqual 2
   }
 }
