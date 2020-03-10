bf8cacfca8092a72957d51cefe995a6462fa451b
objektwerks <objektwerks@runbox.com>
2020-03-10 13:16:40 -0400

    added partitionby test

--- src/test/scala/spark/PartitionTest.scala
+++ src/test/scala/spark/PartitionTest.scala
@@ -12,17 +12,27 @@ class PartitionTest extends FunSuite with Matchers {
 
   test("partition") {
     dataframe.rdd.partitions.length shouldEqual 8
-    dataframe.write.csv(s"./target/${UUID.randomUUID.toString}-partitioned-numbers")
+    dataframe.write.csv(s"./target/partitioned-numbers-${UUID.randomUUID.toString}")
   }
 
   test("coalesce") {
     val coalesced = dataframe.coalesce(2)
     coalesced.rdd.partitions.length shouldEqual 2
-    coalesced.write.csv(s"./target/${UUID.randomUUID.toString}-coalesced-numbers")
+    coalesced.write.csv(s"./target/coalesced-numbers-${UUID.randomUUID.toString}")
   }
 
   test("repartition") {
     dataframe.repartition(4).rdd.partitions.length shouldEqual 4
     dataframe.repartition(2).rdd.partitions.length shouldEqual 2
+  }
+
+  test("partitionBy") {
+    val dataframe = sparkSession.read.json("./data/person/person.json").cache
+    val outputPath = new java.io.File(s"./target/partitionby-roles-${UUID.randomUUID.toString}").getCanonicalPath
+    dataframe
+      .repartition(2)
+      .write
+      .partitionBy("role")
+      .parquet(outputPath)
   }
 }
