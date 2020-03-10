77a8b270cfa8750f4982c08b188d332941430b0d
objektwerks <objektwerks@runbox.com>
2020-03-10 12:48:29 -0400

    corrected 'dir already exists' error

--- src/test/scala/spark/PartitionTest.scala
+++ src/test/scala/spark/PartitionTest.scala
@@ -1,29 +1,27 @@
 package spark
 
+import java.util.UUID
+
 import org.scalatest.{FunSuite, Matchers}
 
 class PartitionTest extends FunSuite with Matchers {
   import SparkInstance._
   import sparkSession.implicits._
 
+  val dataframe = (1 to 10).toList.toDF("number")
+
   test("partition") {
-    val dataframe = (1 to 10).toList.toDF("number")
     dataframe.rdd.partitions.length shouldEqual 8
-    dataframe.write.csv("./target/partitioned-numbers")
+    dataframe.write.csv(s"./target/${UUID.randomUUID.toString}-partitioned-numbers")
   }
 
   test("coalesce") {
-    val dataframe = (1 to 10).toList.toDF("number")
-    dataframe.rdd.partitions.length shouldEqual 8
     val coalesced = dataframe.coalesce(2)
     coalesced.rdd.partitions.length shouldEqual 2
-    coalesced.write.csv("./target/coalesced-numbers")
+    coalesced.write.csv(s"./target/${UUID.randomUUID.toString}-coalesced-numbers")
   }
 
   test("repartition") {
-    val dataframe = (1 to 10).toList.toDF("number")
-    dataframe.rdd.partitions.length shouldEqual 8
-    dataframe.repartition(6).rdd.partitions.length shouldEqual 6
     dataframe.repartition(4).rdd.partitions.length shouldEqual 4
     dataframe.repartition(2).rdd.partitions.length shouldEqual 2
   }
