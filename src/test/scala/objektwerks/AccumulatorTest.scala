22921d6a8928dea403d7ef37c3d7bcf3053009be
objektwerks <objektwerks@runbox.com>
2020-08-20 11:05:05 -0400

    added name to accumulator test

--- src/test/scala/objektwerks/AccumulatorTest.scala
+++ src/test/scala/objektwerks/AccumulatorTest.scala
@@ -9,18 +9,21 @@ class AccumulatorTest extends AnyFunSuite with Matchers {
   import SparkInstance._
 
   test("accumulator") {
-    val longAcc = sparkContext.longAccumulator
+    val longAcc = sparkContext.longAccumulator("longAcc")
     longAcc.add(1)
+    longAcc.name.get shouldBe "longAcc"
     longAcc.value shouldBe 1
 
-    val doubleAcc = sparkContext.doubleAccumulator
+    val doubleAcc = sparkContext.doubleAccumulator("doubleAcc")
     doubleAcc.add(1.0)
+    doubleAcc.name.get shouldBe "doubleAcc"
     doubleAcc.value shouldBe 1.0
 
-    val collectionAcc = sparkContext.collectionAccumulator[Int]
+    val collectionAcc = sparkContext.collectionAccumulator[Int]("collectionAcc")
     collectionAcc.add(1)
     collectionAcc.add(2)
     collectionAcc.add(3)
+    collectionAcc.name.get shouldBe "collectionAcc"
     collectionAcc.value.asScala.sum shouldEqual 6
   }
 }
